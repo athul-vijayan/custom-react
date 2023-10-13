5d6868a18b2b6ba6aef652b21b536f7ee787c386
Athul Vijayan <web.wizard.athul@gmail.com>
2023-10-14 02:18:25 +0530

    element injector ready

--- customreact.js
+++ customreact.js
@@ -1,12 +1,23 @@
 
-customRender=(element, container)=>{
-    const domElement= document.createElement(element.type);
-    domElement.innerHTML=element.children;
-    domElement.setAttribute('href',element.props.href);
-    domElement.setAttribute('target',element.props.target);
+// customRender=(element, container)=>{
+//     const domElement= document.createElement(element.type);
+//     domElement.innerHTML=element.children;
+//     domElement.setAttribute('href',element.props.href);
+//     domElement.setAttribute('target',element.props.target);
 
-    container.appendChild(domElement);
+//     container.appendChild(domElement);
+// }
+
+customRender=(element, container)=>{
+    const domElement=document.createElement(element.type);
+    domElement.innerHTML=element.children;
+    for (const prop in element.props) {
+        domElement.setAttribute(prop, element.props.prop)
+            
+        }
+        container.appendChild(domElement);
 }
+
 
 const reactElement={
     type:'a',
@@ -16,6 +27,12 @@ const reactElement={
     },
     children:'Click Me To Visit Google'
 }
+
+myfn=function(a,b){
+    return(a+b)
+}
+var result = myfn(1,2);
+console.log(result)
 
 const mainContainer=document.querySelector('#root')
 
